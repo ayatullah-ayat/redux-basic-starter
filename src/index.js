7ee457c6104a16011ec8af738af74bb32379f487
ayatullah-ayat <56090060+ayatullah-ayat@users.noreply.github.com>
2022-08-22 09:58:04 +0600

    redux store used

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './immutable';
+import store  from './store';
+
+
+
+
+store.dispatch({
+    type: 'BUG_ADDED',
+    payload: {
+        description: 'Bug1'
+    }
+})
+console.log(store.getState());
